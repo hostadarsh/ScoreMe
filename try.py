caa5707d95e4e8c6a4eb766c13e2e95678e6b3ce
Adarsh Kumar <adarsh.kumar.ug21@nsut.ac.in>
2025-03-26 14:56:43 +0530

    Done

--- try.py
+++
@@ -1,9 +0,0 @@
-import fitz
-
-pdf_file = "test3.pdf"
-doc = fitz.open(pdf_file)
-
-for page_num, page in enumerate(doc, start=1):
-    text = page.get_text("text")
-    print(f"\n--- Page {page_num} Text ---\n")
-    print(text)
